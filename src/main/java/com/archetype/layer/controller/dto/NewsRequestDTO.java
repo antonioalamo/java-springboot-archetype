cb2ed14bfcb9bb05dbca2a74c4d17a2d6e9e8d6f
newvancity <antonio.alamo.cl@gmail.com>
2025-10-21 21:51:48 -0300

    clean but working

--- src/main/java/com/archetype/layer/controller/dto/NewsRequestDTO.java
+++ src/main/java/com/archetype/layer/controller/dto/NewsRequestDTO.java
@@ -1,8 +1,13 @@
 package com.archetype.layer.controller.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 public class NewsRequestDTO {
     private String title;
     private String content;
@@ -12,30 +17,6 @@ public class NewsRequestDTO {
     private List<Source> sources = new ArrayList<Source>();
 
     public NewsRequestDTO() {}
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-    public String getGuid() { return guid; }
-    public void setGuid(String guid) { this.guid = guid; }
-
-    public String getLink() { return link; }
-    public void setLink(String link) { this.link = link; }
-
-    public Double getGrade() { return grade; }
-    public void setGrade(Double grade) { this.grade = grade; }
-
-    public List<Source> getSources() {
-        return sources;
-    }
-
-    public void setSources(List<Source> sources) {
-        this.sources.addAll(sources);
-    }
-
 
     public record Source(String id, String name, String description){
     }
--- src/main/java/com/archetype/layer/controller/dto/NewsResponseDTO.java
+++ src/main/java/com/archetype/layer/controller/dto/NewsResponseDTO.java
@@ -4,9 +4,14 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Max;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 import java.util.ArrayList;
 
+@Setter
+@Getter
 public class NewsResponseDTO {
     private String id;
 
@@ -32,8 +37,6 @@ public class NewsResponseDTO {
 
     private String errorMessage;
 
-    public NewsResponseDTO() {}
-
     public NewsResponseDTO(String id, String title, String content, String guid, String link, Double grade,
                            List<Source> sources) {
         this.id = id;
@@ -54,35 +57,6 @@ public class NewsResponseDTO {
         this.link = null;
         this.grade = null;
         this.errorMessage = errorMessage;
-    }
-
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-    public String getGuid() { return guid; }
-    public void setGuid(String guid) { this.guid = guid; }
-
-    public String getLink() { return link; }
-    public void setLink(String link) { this.link = link; }
-
-    public Double getGrade() { return grade; }
-    public void setGrade(Double grade) { this.grade = grade; }
-
-    public String getErrorMessage() { return errorMessage; }
-    public void setErrorMessage(String errorMessage) { this.errorMessage = errorMessage; }
-
-    public List<NewsResponseDTO.Source> getSources() {
-        return sources;
-    }
-
-    public void setSources(List<NewsResponseDTO.Source> sources) {
-        this.sources.addAll(sources);
     }
 
     public record Source(String id, String name, String description){
